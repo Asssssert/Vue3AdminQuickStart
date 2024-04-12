eb562351524ac8fcdfddcde57f4f77412c074f2a
Asssert <670182350@qq.com>
2024-04-13 02:41:01 +0800

    1、修改权限和菜单内容（反了

--- src/views/setting/MenuList.vue
+++ src/views/setting/MenuList.vue
@@ -1,26 +1,25 @@
 <script setup lang="ts">
 import { reactive, ref } from 'vue'
-
 const data = reactive(
     {
         total: 50, page: 1, size: 10,
         data: [
             {
-                id: 1, title: "公有权限", path: "public", method: "", parentId: 0, auth: "", createTime: '2022-01-01', type: 0, remark: "", child: [
-                    { id: 2, title: "登录", path: "/public/login", auth: "auth", method: "POST", parentId: 1, createTime: '2022-01-01', type: 1, remark: "", child: [] },
-                    { id: 3, title: "注销", path: "/public/logout", auth: "auth", method: "GET", parentId: 1, createTime: '2022-01-01', type: 1, remark: "", child: [] },
-                    { id: 4, title: "获取自身基本信息", path: "/public/get/info", auth: "auth", method: "GET", parentId: 1, createTime: '2022-01-01', type: 1, remark: "", child: [] },
-                    { id: 5, title: "修改自身基本信息", path: "/public/get/info", auth: "auth", method: "POST", parentId: 1, createTime: '2022-01-01', type: 1, remark: "", child: [] },
-                ]
+                id: 1, title: '主页', path: "index", icon: "", child: [
+                    { id: 2, icon: "", title: "大屏", path: "/home/index" },
+                ],
+
             },
             {
-                id: 6, title: "用户权限", path: "user", method: "", parentId: 0, createTime: '2022-01-01', type: 0, remark: "", child: [
-                    { id: 7, title: "根据ID获取用户信息", path: "/user/get/*", auth: "user:get", method: "GET", parentId: 6, createTime: '2022-01-01', type: 1, remark: "", child: [] },
-                    { id: 8, title: "分页查找用户列表", path: "/user/page", auth: "user:page", method: "GET", parentId: 6, createTime: '2022-01-01', type: 1, remark: "", child: [] },
-                    { id: 9, title: "根据ID修改用户信息", path: "/user/upd", auth: "user:upd", method: "POST", parentId: 6, createTime: '2022-01-01', type: 1, remark: "", child: [] },
-                    { id: 10, title: "根据ID删除用户", path: "/user/del/*", auth: "user:del", method: "GET", parentId: 6, createTime: '2022-01-01', type: 1, remark: "", child: [] },
+                id: 5, icon: "", title: "系统设置", path: "setting", child: [
+                    { id: 6, icon: "", title: "用户列表", path: "/home/userList" },
+                    { id: 7, icon: "", title: "角色列表", path: "/home/roleList" },
+                    { id: 8, icon: "", title: "权限列表", path: "/home/permissionList" },
+                    { id: 9, icon: "", title: "菜单列表", path: "/home/menuList" },
+
                 ]
             }
+
         ]
     }
 )
@@ -51,18 +50,9 @@ const handleCurrentChange = (val: number) => {
                         
                         <el-table :empty-text="'暂无数据'" :data="props.row.child" :stripe="true" :highlight-current-row="true">
                             <el-table-column align="center" header-align="center" prop="id" label="编号" />
-                            <el-table-column align="center" header-align="center" prop="title" label="接口名称" />
-                            <el-table-column align="center" header-align="center" prop="path" label="接口地址" />
-                            <el-table-column align="center" header-align="center" prop="method" label="请求方式" />
-                            <el-table-column align="center" header-align="center" prop="auth" label="权限" />
-                            <el-table-column align="center" header-align="center" prop="createTime" label="创建时间" />
-                            <el-table-column align="center" header-align="center" prop="type" label="权限类型" >
-                                <template #default="scope">
-                                    <el-tag v-if="scope.row.type === 0" type="success">菜单</el-tag>
-                                    <el-tag v-else type="warning">接口</el-tag>
-                                </template>
-                            </el-table-column>
-                            <el-table-column align="center" header-align="center" prop="remark" label="备注" />
+                            <el-table-column align="center" header-align="center" prop="title" label="标题" />
+                            <el-table-column align="center" header-align="center" prop="path" label="链接" />
+                            <el-table-column align="center" header-align="center" prop="icon" label="图标" />
                             <el-table-column align="center" header-align="center" label="操作" fixed="right" width="200">
                                 <template #default="scope">
                                     <el-button link type="primary" @click="handleEdit(scope.row)">编辑</el-button>
@@ -74,24 +64,15 @@ const handleCurrentChange = (val: number) => {
                 </template>
             </el-table-column>
             <el-table-column align="center" header-align="center" prop="id" label="编号" />
-                <el-table-column align="center" header-align="center" prop="title" label="接口名称" />
-                <el-table-column align="center" header-align="center" prop="path" label="接口地址" />
-                <el-table-column align="center" header-align="center" prop="method" label="请求方式" />
-                <el-table-column align="center" header-align="center" prop="auth" label="权限" />
-                <el-table-column align="center" header-align="center" prop="createTime" label="创建时间" />
-                <el-table-column align="center" header-align="center" prop="type" label="权限类型" >
-                    <template #default="scope">
-                        <el-tag v-if="scope.row.type === 0" type="success">菜单</el-tag>
-                        <el-tag v-else type="warning">接口</el-tag>
-                    </template>
-                </el-table-column>
-                <el-table-column align="center" header-align="center" prop="remark" label="备注" />
-                <el-table-column align="center" header-align="center" label="操作" fixed="right" width="200">
-                    <template #default="scope">
-                        <el-button link type="primary" @click="handleEdit(scope.row)">编辑</el-button>
-                        <el-button link type="primary" @click="handleDel(scope.row)">删除</el-button>
-                    </template>
-                </el-table-column>
+            <el-table-column align="center" header-align="center" prop="title" label="标题" />
+            <el-table-column align="center" header-align="center" prop="path" label="链接" />
+            <el-table-column align="center" header-align="center" prop="icon" label="图标" />
+            <el-table-column align="center" header-align="center" label="操作" fixed="right" width="200">
+                <template #default="scope">
+                    <el-button link type="primary" @click="handleEdit(scope.row)">编辑</el-button>
+                    <el-button link type="primary" @click="handleDel(scope.row)">删除</el-button>
+                </template>
+            </el-table-column>
         </el-table>
 
         <el-pagination v-model:current-page="data.page" v-model:page-size="data.size" small="small"
